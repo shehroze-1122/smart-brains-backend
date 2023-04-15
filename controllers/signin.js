7d8bf44fd1bc931d99b257af0ae639a7686820a8
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2023-04-15 19:49:21 +0500

    improve error handling

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,17 +7,21 @@ const handleSignIn = (req, res, pg, bcrypt) =>
          }
         pg.select('hash', 'email').from('login').where('email','=', req.body.email)
         .then(data=>{
+            if(!data.length) return res.status('404').json({ error: "Wrong email or password"})
             bcrypt.compare(req.body.password, data[0].hash , function(err, result) {
                 if(result){
                     pg.select('*').from('users').where('email', '=', data[0].email)
                     .then(user=>res.json(user[0]))
                 }else{
-                    res.status('400').json("failed")
+                    res.status('400').json({error: "Wrong email or password"})
                 }
         });
 
         })
-        .catch(err=> res.status('400').json("failed"))
+        .catch(err=> {
+            console.log(err)
+            res.status('500').json({error: "Something went wrong"})
+        })
     }
 
 module.exports={
